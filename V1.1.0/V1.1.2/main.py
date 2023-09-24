8428184cc86bf330cc76788e81019ee419e5e584
Philip Julian <pjulianofficial@gmail.com>
2023-09-24 12:47:08 -0500

    save

--- V1.1.0/V1.1.2/main.py
+++ V1.1.0/V1.1.2/main.py
@@ -2,4 +2,8 @@
 import html_functions
 ########################################
 
-print(html_functions.craft_url(0))
+# print(html_functions.scrape_website(offset=1))
+print(html_functions.scrape_website(start=0, offset=2))
+
+#added consolidate list capabilities
+#fixed bug where stats werent able to be gathered for current year
